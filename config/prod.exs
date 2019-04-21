cd29218d58d73e464a486eeaec4f20c77d02d4b7
Steve Wagner <ciroque@live.com>
2019-04-21 11:15:10 -0700

    Use IPv4

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :stochastica, StochasticaWeb.Endpoint,
   load_from_system_env: true,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  http: [port: {:system, "PORT"}, ip: {0, 0, 0, 0}],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("STOCHASTICA_SECRET_KEY_BASE")
 
