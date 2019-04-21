a6539f4aeff91c73926cfba80fa9b73c33086f9d
Steve Wagner <ciroque@live.com>
2019-04-21 11:44:22 -0700

    Adds secret key base

--- config/prod.exs
+++ config/prod.exs
@@ -6,4 +6,7 @@ config :stochastica, StochasticaWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("STOCHASTICA_SECRET_KEY_BASE")
 
+config :stochastica, StochasticaWeb.Endpoint,
+       secret_key_base: "6kTV5ExO0RWjfBq6SC/E61pyGRnS9+dB0jI3amI6OYpwZK87dPdRZ2Jh2S6y3a8r"
+
 config :logger, level: :info
