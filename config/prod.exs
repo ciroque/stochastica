e6c266c90f3ef46a9ae2f707eabeef1b93db8a6f
Steve Wagner <ciroque@live.com>
2019-06-14 09:03:00 -0700

    Correct Copy Pasta error

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :stochastica, StochasticaWeb.Endpoint,
-  on_init: {Exertrax.Web.Endpoint, :load_from_system_env, []},
+  on_init: {StochasticaWeb.Endpoint, :load_from_system_env, []},
   http: [port: {:system, "PORT"}, ip: {0, 0, 0, 0}],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("STOCHASTICA_SECRET_KEY_BASE"),
